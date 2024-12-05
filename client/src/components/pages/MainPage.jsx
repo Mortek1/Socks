0f19f48ec77cf187733ace1b8a065a9791a2b79b
sasha509 <abc509@yandex.ru>
2024-12-05 16:39:15 +0300

    main page

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -1,16 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Button, Row } from 'react-bootstrap';
-
-
-export default function MainPage() {
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Button, Row, Col } from "react-bootstrap";
+import Footer from "../ui/Footer";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+export default function MainPage({ user }) {
+  // const [show, setShow] = useState(false);
 
   return (
-    <>
-    <Row className='mt-3'>
-    </Row>
+    <div className="d-flex flex-column min-vh-100">
+      <header className="bg-light py-4 text-center">
+        {/* <h1>Welcome, {user?.name || "Guest"}!</h1> */}
+      </header>
 
-</>
-  )
+      <main className="flex-grow-1">
+        <Container className="py-5">
+          <Row className="justify-content-center text-center">
+            <Col md={6}>
+              {user.status !== "logged" ? (
+                <h1 className="display-1">Привет!</h1>
+              ) : (
+                <h1 className="display-1">Привет, {user.data.name}!</h1>
+              )}
+              <Row>
+                {user.status !== "logged" ? (
+                  <>
+                    <p>Давай задизайним твои носки</p>
+                    <p>Но сперва зарегистрируйся</p>
+                  </>
+                ) : (
+                  <>
+                    <p>Отлично, тебе наскучили обычные серые носки!?</p>
+                    <NavLink to="/" className="nav-link">
+                      Переходи сюда и создай свои носки с индивидуальным и ярким
+                      дизайном!
+                    </NavLink>
+                  </>
+                )}
+              </Row>
+              <div className="mt-4">
+                <Link to={"/account/new"}>
+                  <Button variant="outline-secondary" className="me-2">
+                    Sign up
+                  </Button>
+                </Link>
+                <Link to={"/account/login"}>
+                  <Button variant="outline-secondary">Log in</Button>
+                </Link>
+              </div>
+              <Row className="mt-4"></Row>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
